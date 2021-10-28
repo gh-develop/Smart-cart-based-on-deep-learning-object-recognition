be8480f931a822b6bc2087745b44c8170db9c51a
GyuHyeon <suintyu@naver.com>
2021-10-28 23:47:38 +0900

    upload cfg, data, names, weights

--- back-end/server/tcp_opencv.py
+++ back-end/server/tcp_opencv.py
@@ -1,7 +1,9 @@
+import socket
 import cv2
+import pickle
+import struct
 import numpy as np
 import pymysql
-import socket
 
 # Reset SQL and Connect
 
@@ -30,8 +32,8 @@ net = cv2.dnn.readNetFromDarknet("/home/suintyu/Downloads/yolov4-tiny-custom_zer
                                  r"/home/suintyu/Downloads/yolov4-tiny-custom_zero_kancho_best.weights")
 # Change here for custom classes for trained model
 
-classes = ['curry', 'rice', 'CupNoodle', 'ChicChoc', 'powerade', 'zerocider', 'kancho']
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+classes = ['curry', 'rice', 'CupNoodle',
+           'ChicChoc', 'powerade', 'zerocider', 'kancho']
 
 # Initial Parameter###
 # object detection
@@ -62,8 +64,6 @@ past_weight = 0
 
 # Connect With Socket###
 
-# socket receive buffer size
-
 
 def recvall(sock, count):
     buf = b''
@@ -76,42 +76,47 @@ def recvall(sock, count):
     return buf
 
 
-# server ip, port
-TCP_IP = '10.178.0.3'
-TCP_PORT = 3389
+ip = '10.178.0.3'  # ip 주소
+port = 3389  # port 번호
 
-# TCP connect wait
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
-s.bind((TCP_IP, TCP_PORT))
-print('Socket bind complete')
-s.listen(True)
-print('Socket now listening')
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 소켓 객체를 생성
+s.bind((ip, port))  # 바인드(bind) : 소켓에 주소, 프로토콜, 포트를 할당
+s.listen(10)  # 연결 수신 대기 상태(리스닝 수(동시 접속) 설정)
+print('클라이언트 연결 대기')
 
-# conn: socket object, addr:socket address
+# 연결 수락(클라이언트 소켓 주소를 반환)
 conn, addr = s.accept()
+print(addr)  # 클라이언트 주소 출력
 
-# Processing###
-while 1:
-    length = recvall(conn, 16)
+data = b""  # 수신한 데이터를 넣을 변수
+payload_size = struct.calcsize(">L")
 
-    if length == None:
-        s.close()
-        print('Socket connection terminate')
-        break
-    # image data receive
-    stringData = recvall(conn, int(length))
-    data = np.fromstring(stringData, dtype='uint8')
-    img = cv2.imdecode(data, 1)
+while True:
+    # 프레임 수신
+    while len(data) < payload_size:
+        data += conn.recv(4096)
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    print("Frame Size : {}".format(msg_size))  # 프레임 크기 출력
+
+    # 역직렬화(de-serialization) : 직렬화된 파일이나 바이트를 원래의 객체로 복원하는 것
+    # 직렬화되어 있는 binary file로 부터 객체로 역직렬화
+    frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)  # 프레임 디코딩
 
     # weight data receive
-    length2 = recvall(conn, 8)
-    weighData = recvall(conn, int(length2))
-    weight = int(weighData)
+    #length2 = recvall(conn, 8)
+    #weighData = recvall(conn, int(length2))
+    weight = int(200)  # int(weighData)
     # sensor weight print
     print("weight: {}".format(weight))
 
-    ### weight stable process
+    # weight stable process
 
     # weight difference detect
     past_weight = current_weight
@@ -142,8 +147,9 @@ while 1:
         # change weight print
         print("weight change detection: {}".format(check_weight))
 
-    hight, width, _ = img.shape
-    blob = cv2.dnn.blobFromImage(img, 1 / 255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+    hight, width, _ = frame.shape
+    blob = cv2.dnn.blobFromImage(
+        frame, 1 / 255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
 
     net.setInput(blob)
 
@@ -151,9 +157,11 @@ while 1:
 
     layerOutputs = net.forward(output_layers_name)
 
+    """
     boxes = []
     confidences = []
     class_ids = []
+
 
     for output in layerOutputs:
         for detection in output:
@@ -172,7 +180,7 @@ while 1:
                 class_ids.append(class_id)
 
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, .5, .4)
-
+    """
     boxes = []
     confidences = []
     class_ids = []
@@ -205,8 +213,9 @@ while 1:
             label = str(classes[class_ids[i]])
             confidence = str(round(confidences[i], 2))
             color = colors[i]
-            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(img, label + " " + confidence, (x, y + 400), font, 2, color, 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, label + " " + confidence,
+                        (x, y + 400), font, 2, color, 2)
 
             # object detection update to check_detection
             check_detection.add(label)
@@ -215,46 +224,58 @@ while 1:
                 print("detection set: {}".format(check_detection))
 
         # if weight decrease
-        if 'curry' in check_detection and (-210 in check_weight or -220 in check_weight or -230 in check_weight):
+        if 'curry' in check_detection and (
+                -210 in check_weight or -220 in check_weight or -230 in check_weight or -240 in check_weight):
             out_cu += 1
         # if weight increse
-        elif 'curry' in check_detection and (210 in check_weight or 220 in check_weight or 230 in check_weight or 240 in check_weight):
+        elif 'curry' in check_detection and (
+                210 in check_weight or 220 in check_weight or 230 in check_weight or 240 in check_weight):
             in_cu += 1
 
-        elif 'rice' in check_detection and (-200 in check_weight or -210 in check_weight):
+        elif 'rice' in check_detection and (-200 in check_weight or -210 in check_weight or -220 in check_weight):
             out_ri += 1
         elif 'rice' in check_detection and (200 in check_weight or 210 in check_weight or 220 in check_weight):
             in_ri += 1
 
-        elif 'CupNoodle' in check_detection and (-40 in check_weight or -50 in check_weight or -60 in check_weight):
+        elif 'CupNoodle' in check_detection and (
+                -30 in check_weight or -40 in check_weight or -50 in check_weight or -60 in check_weight or -70 in check_weight):
             out_cup += 1
-        elif 'CupNoodle' in check_detection and (40 in check_weight or 50 in check_weight or 60 in check_weight or 70 in check_weight):
+        elif 'CupNoodle' in check_detection and (
+                30 in check_weight or 40 in check_weight or 60 in check_weight or 70 in check_weight):
             in_cup += 1
 
-        elif 'ChicChoc' in check_detection and (-150 in check_weight or -160 in check_weight or -170 in check_weight):
+        elif 'ChicChoc' in check_detection and (
+                -150 in check_weight or -160 in check_weight or -170 in check_weight or -180 in check_weight):
             out_chic += 1
-        elif 'ChicChoc' in check_detection and (150 in check_weight or 160 in check_weight or 170 in check_weight or 180 in check_weight):
+        elif 'ChicChoc' in check_detection and (
+                150 in check_weight or 160 in check_weight or 170 in check_weight or 180 in check_weight):
             in_chic += 1
 
-        elif 'powerade' in check_detection and (-360 in check_weight or -370 in check_weight or -380 in check_weight):
+        elif 'powerade' in check_detection and (
+                -360 in check_weight or -370 in check_weight or -380 in check_weight or -390 in check_weight):
             out_power += 1
-        elif 'powerade' in check_detection and (360 in check_weight or 370 in check_weight or 380 in check_weight or 390 in check_weight):
+        elif 'powerade' in check_detection and (
+                360 in check_weight or 370 in check_weight or 380 in check_weight or 390 in check_weight):
             in_power += 1
 
-        elif 'zerocider' in check_detection and (-250 in check_weight or -260 in check_weight or -270 in check_weight):
+        elif 'zerocider' in check_detection and (
+                -250 in check_weight or -260 in check_weight or -270 in check_weight or -280 in check_weight):
             out_zero += 1
-        elif 'zerocider' in check_detection and (250 in check_weight or 260 in check_weight or 270 in check_weight or 280 in check_weight):
+        elif 'zerocider' in check_detection and (
+                250 in check_weight or 260 in check_weight or 270 in check_weight or 280 in check_weight):
             in_zero += 1
 
-        elif 'kancho' in check_detection and (-90 in check_weight or -100 in check_weight or -110 in check_weight):
+        elif 'kancho' in check_detection and (
+                -90 in check_weight or -100 in check_weight or -110 in check_weight or -120 in check_weight):
             out_kan += 1
-        elif 'kancho' in check_detection and (90 in check_weight or 100 in check_weight or 110 in check_weight or 120 in check_weight):
+        elif 'kancho' in check_detection and (
+                90 in check_weight or 100 in check_weight or 110 in check_weight or 120 in check_weight):
             in_kan += 1
 
         # check out algorithm
         # if 'curry' in check_detection and weight == -100:
         if out_cu >= 1:
-            sql = "UPDATE Food SET number = number-1 WHERE product = 'curry';"
+            sql = "UPDATE USERBASKET SET classNum = classNum-1 WHERE className = 'curry';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***curry number decrease upload***')
             # initialize
@@ -264,11 +285,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'rice' in check_detection and weight == -200:
         if out_ri >= 1:
-            sql = "UPDATE Food SET number = number-1 WHERE product = 'rice';"
+            sql = "UPDATE USERBASKET SET classNum = classNum-1 WHERE className = 'rice';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***rice number decrease upload***')
             out_ri = 0
@@ -277,11 +298,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'CupNoodle' in check_detection and weight == -300:
         if out_cup >= 1:
-            sql = "UPDATE Food SET number = number-1 WHERE product = 'CupNoodle';"
+            sql = "UPDATE USERBASKET SET classNum = classNum-1 WHERE className = 'cupnoodle';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***cupnoodle number decrease upload***')
             out_cup = 0
@@ -290,11 +311,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'ChicChoc' in check_detection and weight == -400:
         if out_chic >= 1:
-            sql = "UPDATE Food SET number = number-1 WHERE product = 'ChicChoc';"
+            sql = "UPDATE USERBASKET SET classNum = classNum-1 WHERE className = 'chicchoc';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***chicchoc number decrease upload***')
             out_chic = 0
@@ -303,11 +324,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'powerade' in check_detection and weight == -500:
         if out_power >= 1:
-            sql = "UPDATE Food SET number = number-1 WHERE product = 'powerade';"
+            sql = "UPDATE USERBASKET SET classNum = classNum-1 WHERE className = 'powerade';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***powerade number decrease upload***')
             out_power = 0
@@ -316,11 +337,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'zerocider' in check_detection and weight == -600:
         if out_zero >= 1:
-            sql = "UPDATE Food SET number = number-1 WHERE product = 'zerocider';"
+            sql = "UPDATE USERBASKET SET classNum = classNum-1 WHERE className = 'zerocider';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***zerocider number decrease upload***')
             out_zero = 0
@@ -329,11 +350,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'kancho' in check_detection and weight == -700:
         if out_kan >= 1:
-            sql = "UPDATE Food SET number = number-1 WHERE product = 'kancho';"
+            sql = "UPDATE USERBASKET SET classNum = classNum-1 WHERE className = 'kancho';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***kancho number decrease upload***')
             out_kan = 0
@@ -342,12 +363,12 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # check in algorithm
         # if 'curry' in check_detection and weight == 100:
         if in_cu >= 1:
-            sql = "UPDATE Food SET number = number+1 WHERE product = 'curry';"
+            sql = "UPDATE USERBASKET SET classNum = classNum+1 WHERE className = 'curry';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***curry number increase upload***')
             in_cu = 0
@@ -356,11 +377,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'rice' in check_detection and weight == 200:
         if in_ri >= 1:
-            sql = "UPDATE Food SET number = number+1 WHERE product = 'rice';"
+            sql = "UPDATE USERBASKET SET classNum = classNum+1 WHERE className = 'rice';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***rice number increase upload***')
             in_ri = 0
@@ -369,11 +390,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'CupNoodle' in check_detection and weight == 300:
         if in_cup >= 1:
-            sql = "UPDATE Food SET number = number+1 WHERE product = 'CupNoodle';"
+            sql = "UPDATE USERBASKET SET classNum = classNum+1 WHERE className = 'cupnoodle';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***cupnoodle number increase upload***')
             in_cup = 0
@@ -382,11 +403,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'ChicChoc' in check_detection and weight == 400:
         if in_chic >= 1:
-            sql = "UPDATE Food SET number = number+1 WHERE product = 'ChicChoc';"
+            sql = "UPDATE USERBASKET SET classNum = classNum+1 WHERE className = 'chicchoc';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***chicchoc number increase upload***')
             in_chic = 0
@@ -395,11 +416,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'powerade' in check_detection and weight == 500:
         if in_power >= 1:
-            sql = "UPDATE Food SET number = number+1 WHERE product = 'powerade';"
+            sql = "UPDATE USERBASKET SET classNum = classNum+1 WHERE className = 'powerade';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***powerade number increase upload***')
             in_power = 0
@@ -408,11 +429,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'zerocider' in check_detection and weight == 600:
         if in_zero >= 1:
-            sql = "UPDATE Food SET number = number+1 WHERE product = 'zerocider';"
+            sql = "UPDATE USERBASKET SET classNum = classNum+1 WHERE className = 'zerocider';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***zerocider number increase upload***')
             in_zero = 0
@@ -421,11 +442,11 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
         # elif 'kancho' in check_detection and weight == 700:
         if in_kan >= 1:
-            sql = "UPDATE Food SET number = number+1 WHERE product = 'kancho';"
+            sql = "UPDATE USERBASKET SET classNum = classNum+1 WHERE className = 'kancho';"
             check_detection = set()  # set \ucd08\uae30\ud654
             print('***kancho number increase upload***')
             in_kan = 0
@@ -434,10 +455,13 @@ while 1:
             index = 0
             minus = 0
             cursor.execute(sql)
-            Food.commit()
+            USERBASKET.commit()
 
-    cv2.imshow('img', img)
-    if cv2.waitKey(1) == ord('q'):
+    # 영상 출력
+    cv2.imshow('TCP_Frame_Socket', frame)
+
+    # 1초 마다 키 입력 상태를 받음
+    if cv2.waitKey(1) == ord('q'):  # q를 입력하면 종료
         break
-cap.release()
+
 cv2.destroyAllWindows()
